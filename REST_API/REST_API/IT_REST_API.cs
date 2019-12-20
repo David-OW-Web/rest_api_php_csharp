9ac6d5b66d49f7ed0e9c9f2f81366d8a98004c50
David <david.peric@kauz.ch>
2019-12-20 10:17:42 +0100

    C# Classlibrary added

---
+++ REST_API/REST_API/IT_REST_API.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REST_API
+{
+    interface IT_REST_API
+    {
+    }
+}
